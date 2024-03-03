9184dd6939f875b5a1bf04eaf48b953e1082d69f
jonnie254 <johnkamanu254@proton.me>
2024-03-03 16:21:54 +0300

    added coded

--- app.js
+++ app.js
@@ -9,3 +9,19 @@ const hours = date.getHours();
 const minutes = date.getMinutes();
 
 const formattedDate = `${day}/${month}/${year}`;
+currentDateParagraph.textContent = formattedDate;
+dateOptionsSelectElement.addEventListener("change", (event) => {
+  switch (dateOptionsSelectElement.value) {
+    case "yyyy-mm-dd":
+      currentDateParagraph.textContent = formattedDate
+        .split("-")
+        .reverse()
+        .join("-");
+      break;
+    case "mm-dd-yyyy-h-mm":
+      currentDateParagraph.textContent = `${month}-${day}-${year}-${hours}-${minutes}`;
+      break;
+    default:
+      currentDateParagraph.textContent = formattedDate;
+  }
+});
