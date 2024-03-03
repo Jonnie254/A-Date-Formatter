af219a0a4ab3db4e881cb3a2b8074986d8248393
jonnie254 <johnkamanu254@proton.me>
2024-03-03 16:09:33 +0300

    added comments

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-const 
+/* we declare the variables*/
+const currentDateParagraph = document.getElementById("current-date");
+const dateOptionsSelectElement = document.getElementById("date-options");
+const date = new Date();
+const day = date.getDate();
+const month = date.getMonth() + 1;
+const year = date.getFullYear();
+const hours = date.getHours();
+const minutes = date.getMinutes();
+
+const formattedDate = `${day}/${month}/${year}`;
